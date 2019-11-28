27573476b79229467b4d7c5a4321ac5aad9853c6
harrychor <58283630+harrychor@users.noreply.github.com>
2019-11-28 19:19:02 +0800

    Create Q2_Substringchecking.java

---
+++ bin/assignment/Q2_Substringchecking.java
@@ -0,0 +1,25 @@
+package assignment;
+
+import java.util.Scanner;
+
+public class Q2_Substringchecking {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		Scanner inputSub = new Scanner(System.in);
+		try {
+		System.out.println("input string1 :");
+		String string1 = input.nextLine();
+		System.out.println("input string2 :");
+		String string2 = inputSub.nextLine();
+
+        if (string1 != null && string2 != null & string2.length() <= string1.length() & string1.contains(string2)) {
+            System.out.println("string1 contains string2");
+        } else {
+        	System.out.println("string1 is not contains to string2");
+        }
+		}catch(Exception e) {
+			System.out.println("which is not a vaild number or string.");
+		}
+	  }
+
+}
