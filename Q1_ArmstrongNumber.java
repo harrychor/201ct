60bb04bd732c658304a49503f0b7bf870002af77
harrychor <58283630+harrychor@users.noreply.github.com>
2019-11-28 14:15:19 +0800

    Update Q1_ArmstrongNumber.java

--- Q1_ArmstrongNumber.java
+++ Q1_ArmstrongNumber.java
@@ -4,9 +4,12 @@ import java.util.Scanner;
 
 public class Q1_ArmstrongNumber {
 	public static void main(String[] args) {
+		
 		Scanner input = new Scanner(System.in);
         int number, originalNumber, remainder, result = 0, n = 0;
+        
         System.out.print("Enter number: ");
+        try {
         number = input.nextInt();
         originalNumber = number;
         
@@ -17,9 +20,15 @@ public class Q1_ArmstrongNumber {
             remainder = originalNumber % 10;
             result += Math.pow(remainder, n);
         }
+		
+		
         if(result == number)
             System.out.println(number + " is an Armstrong number.");
         else
             System.out.println(number + " is not an Armstrong number.");
+        }catch(Exception e) {
+			System.out.println("which is not a vaild number.");
+		}
+        
 }
 }
