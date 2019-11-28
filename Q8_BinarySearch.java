70b0714bb2ce0b6e31baae2b4c5310ded6c1499a
harrychor <58283630+harrychor@users.noreply.github.com>
2019-11-28 16:35:00 +0800

    Update Q8_BinarySearch.java

--- Q8_BinarySearch.java
+++ Q8_BinarySearch.java
@@ -3,29 +3,20 @@ package assignment;
 import java.util.Scanner;
 
 public class Q8_BinarySearch {
-	 // Returns index of x if it is present in arr[l.. 
-    // r], else return -1 
     int binarySearch(int array[], int l, int r, int x) 
     { 
         if (r >= l) { 
             int mid = l + (r - l) / 2; 
   
-            // If the element is present at the 
-            // middle itself 
             if (array[mid] == x) 
                 return mid; 
-  
-            // If element is smaller than mid, then 
-            // it can only be present in left subarray 
+
             if (array[mid] > x) 
                 return binarySearch(array, l, mid - 1, x); 
   
-            // Else the element can only be present 
-            // in right subarray 
             return binarySearch(array, mid + 1, r, x); 
         }   
-        // We reach here when element is not present 
-        // in array 
+ 
         return -1; 
     } 
 
